ebdaf9a1d3ac4669fd20b811ff19c3c2d92034d3
Braian Piazza <braianpiazza@gmail.com>
2022-07-27 22:38:47 -0300

    Subiendo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //If
+        int numeroIf = 1 ;
+
+        if (numeroIf > 0) {
+            System.out.println("Positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("Negativo");
+        } else {
+            System.out.println("Cero");
+        }
+
+        //While
+        int numeroWhile = 0;
+        while (numeroWhile<3) {
+            numeroWhile = numeroWhile + 1;
+            System.out.println(numeroWhile);
+        }
+
+        do {
+            System.out.println(numeroWhile);
+        } while (numeroWhile < 3) ;
+
+        for (int numeroFor = 0;numeroFor <= 3;numeroFor ++) {
+            System.out.println(numeroFor);
+        }
+
+        var estacion = "Invierno";
+
+        switch (estacion) {
+            case "Verano":
+                System.out.println("Es verano");
+                break;
+            case "Primavera" :
+                System.out.println("Es primavera");
+                break;
+            case "Invierno" :
+                System.out.println("Es invierno");
+                break;
+            case "Otoño" :
+                System.out.println("Es otoño");
+                break;
+            default :
+                System.out.println("Estación inexistente");
+
+        }
     }
 }
